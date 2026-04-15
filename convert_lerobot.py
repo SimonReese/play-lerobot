199675318982a18cd5550f19f4d0c2b326eabe67
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-04-16 00:32:59 +0200

    Main script for  manipulating the two formats

---
+++ convert_lerobot.py
@@ -0,0 +1,158 @@
+# 1 open rlbench dataset
+# 2 test data loading
+# 3 convert to lerobot
+
+import os
+import pickle
+import time
+from typing import List
+from numpy import ndarray
+from rlbench_utils.observation import Observation
+from rlbench_utils.observation_config import CameraConfig, ObservationConfig
+import rlbench_utils.utils 
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from PIL import Image
+
+# DATASETS PATHS
+RLBENCH_DATASET_ROOT = "./datasets/rlbench/generated-14-04-11-42"
+TASKS = os.listdir(RLBENCH_DATASET_ROOT)
+
+# CAMERA CONFIGURATIONS
+IMAGE_SIZE = (224, 224)
+IMAGE_SHAPE = (224, 224, 3)
+CAMERA_CONFIG = CameraConfig(
+    image_size= IMAGE_SIZE
+)
+OBS_CONFIG = ObservationConfig(
+    left_shoulder_camera= CAMERA_CONFIG,
+    right_shoulder_camera= CAMERA_CONFIG,
+    overhead_camera= CAMERA_CONFIG,
+    wrist_camera= CAMERA_CONFIG,
+    front_camera= CAMERA_CONFIG
+)
+
+# LERobot features dictionary
+FEATURES_DICT = {
+    "image": {
+        "dtype": "image",
+        "shape": IMAGE_SHAPE,
+        "names": ["width", "height", "channel"],
+    },
+    "wrist_image": {
+        "dtype": "image",
+        "shape": IMAGE_SHAPE,
+        "names": ["width", "height", "channel"],
+    },
+    "state": {
+        "dtype": "float32",
+        "shape": (8,),
+        "names": ["state"],
+    },
+    "actions": {
+        "dtype": "float32",
+        "shape": (7,),
+        "names": ["actions"],
+    }
+}
+
+FRAME_DICT = {
+    "image": ndarray,
+    "wrist_image": ndarray,
+    "state" : ndarray,
+    "actions" : ndarray,
+    "task": str
+}
+
+def main():
+
+    lerobot_dataset = LeRobotDataset.create(
+        repo_id=f"RLBench{time.time()}",
+        fps=10,
+        features=FEATURES_DICT,
+        root=f"datasets/lerobot/test-{time.time()}",
+        robot_type="panda"
+    )
+
+    # Open every task
+    for TASK in TASKS:
+        VARIATIONS = get_variations_ids(RLBENCH_DATASET_ROOT, TASK)
+        # Open every variation
+        for VARIATION in VARIATIONS:
+            EPISODES = get_episodes_number(RLBENCH_DATASET_ROOT, TASK, VARIATION)
+            _temp_task_descr = get_task_description(RLBENCH_DATASET_ROOT, TASK, VARIATION) 
+            # Open every episode
+            for EPISODE in EPISODES:
+                print(f"Processing TASK:{TASK}, VARIATION:{VARIATION}, EP:{EPISODE}")
+                # Get the demo for the episode, since loading all episodes is expensive
+                DEMOS = rlbench_utils.utils.get_stored_demos(
+                    amount=1,
+                    image_paths=False,
+                    dataset_root=RLBENCH_DATASET_ROOT,
+                    variation_number=VARIATION,
+                    task_name=TASK,
+                    obs_config=OBS_CONFIG,
+                    random_selection=False,
+                    from_episode_number=EPISODE
+                )
+                DEMO = DEMOS.pop()
+                # Given an image at timestep t, we want the action to reach next position
+                _prev_obs: Observation
+                for seq, observation in enumerate(DEMO):
+                    if seq == 0:
+                        _prev_obs = observation
+                        continue
+                    
+                    new_pose = observation.gripper_pose
+                    old_pose = _prev_obs.gripper_pose
+
+                    FRAME_DICT["image"] = observation.front_rgb # TODO: Which format?? Now is uint8 rgb with (width, height, channels)
+                    FRAME_DICT["wrist_image"] = observation.wrist_rgb
+                    FRAME_DICT["state"] = observation.joint_positions
+                    FRAME_DICT["actions"] = observation.gripper_pose # TODO: convert to delta xyz
+                    FRAME_DICT["task"] = _temp_task_descr
+
+                    lerobot_dataset.add_frame(FRAME_DICT)
+                    _prev_obs = observation
+                lerobot_dataset.save_episode()
+                
+                
+        
+
+# ----- UTILITY FUNCTIONS -----
+
+def get_task_description(dataset_path: str, task_name:str, variation_id: int, VARIATION_FOLDER_PREFIX = "variation", DESCRIPTIONS_FILENAME = "variation_descriptions.pkl" ) -> str:
+    # TODO: Fix the description in the picke file
+    f = open(os.path.join(dataset_path, task_name, VARIATION_FOLDER_PREFIX + str(variation_id), DESCRIPTIONS_FILENAME ), "rb")
+    obj = pickle.load(f)
+    print(f"Warning: Until fix this task only has {len(obj)} descriptions:\n{obj}")
+    return obj[0]
+
+
+
+# Get list of variations for task
+def get_variations_ids(dataset_path: str, task_name:str, VARIATION_FOLDER_PREFIX = "variation") -> List[int]:
+    # Open variation
+    VARIATION_FOLDER_PREFIX = "variation"
+    variation_folders = os.listdir(os.path.join(dataset_path, task_name))
+    if "all_variations" in variation_folders: variation_folders.remove("all_variations")
+    variation_ids = []
+    for variation in variation_folders:
+        if not os.path.isdir(os.path.join(dataset_path, task_name, variation)): continue
+        if VARIATION_FOLDER_PREFIX not in variation: continue
+        id = variation.removeprefix(VARIATION_FOLDER_PREFIX)
+        variation_ids.append(int(id))
+    return variation_ids
+
+def get_episodes_number(dataset_path: str, task_name:str, variation_id: int, VARIATION_FOLDER_PREFIX = "variation", EPISODES_FOLDER = "episodes",EPISODE_FOLDER_PREFIX = "episode") -> List[int]:
+    episodes_folders = os.listdir(os.path.join(dataset_path, task_name, f"{VARIATION_FOLDER_PREFIX}{variation_id}", EPISODES_FOLDER))
+    episode_ids = []
+    for ep in episodes_folders:
+        if not os.path.isdir(os.path.join(dataset_path, task_name, f"{VARIATION_FOLDER_PREFIX}{variation_id}", EPISODES_FOLDER, ep)): continue
+        if EPISODE_FOLDER_PREFIX not in ep: continue
+        id = ep.removeprefix(EPISODE_FOLDER_PREFIX)
+        episode_ids.append(int(id))
+    episode_ids.sort()
+    return episode_ids
+
+if __name__ == "__main__":
+    main()
