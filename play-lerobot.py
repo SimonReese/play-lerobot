fd85c2e90183d9e787235a5893ad414bf4d447fe
SimonReese <59612841+SimonReese@users.noreply.github.com>
2026-05-18 16:10:08 +0200

    Adds test script

---
+++ play-lerobot.py
@@ -0,0 +1,51 @@
+from lerobot.policies.pi05.modeling_pi05 import PI05Policy
+from lerobot.policies.pi05.configuration_pi05 import PI05Config
+
+from lerobot.policies.factory import make_pre_post_processors
+
+from lerobot.datasets.lerobot_dataset import LeRobotDataset
+from lerobot.configs.types import FeatureType, PolicyFeature, NormalizationMode
+
+
+dataset = LeRobotDataset("lerobot/libero_spatial_image")
+print(dataset.meta.features)
+
+model_id = "lerobot/pi05_base"
+policy = PI05Policy.from_pretrained(model_id).to("cuda").eval()
+print(policy.config.input_features)
+exit()
+print("Changing features")
+assert policy.config.input_features is not None
+old_keys = list(policy.config.input_features.keys())
+for k in old_keys:
+    if policy.config.input_features[k].type == FeatureType.VISUAL:
+        del policy.config.input_features[k]
+
+# Add new mappings
+policy.config.input_features["observation.images.image"] = PolicyFeature(type=FeatureType.VISUAL, shape=(3,256, 256))
+policy.config.input_features["observation.images.wrist_image"] = PolicyFeature(type=FeatureType.VISUAL, shape=(3, 256, 256))
+
+policy.config.normalization_mapping["STATE"] = NormalizationMode.MEAN_STD
+policy.config.normalization_mapping["ACTION"] = NormalizationMode.MEAN_STD
+
+policy.config.output_features["action"] = PolicyFeature(FeatureType.ACTION, shape=(7,))
+
+pre, post = make_pre_post_processors(
+    policy.config,
+    dataset_stats=dataset.meta.stats #type: ignore
+)
+
+
+ep_index = 0
+start_idx = dataset.meta.episodes["dataset_from_index"][ep_index]
+end_idx = dataset.meta.episodes["dataset_to_index"][ep_index]
+
+for f in range(start_idx, end_idx):
+    frame = dict(dataset[f])
+    batch = pre(frame)
+    #print(batch.keys())
+    prediction = policy.select_action(batch)
+    processed = post(prediction)
+    print(processed)
+    exit()
+
